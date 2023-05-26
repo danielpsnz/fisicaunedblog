fcf6127dc8b2e21f998a877b6053a556b4f7fb01
Daniel Pérez <dpdaniperezsanz@gmail.com>
2023-05-26 19:13:11 +0100

    updated app js file, added dark-mode css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+
+import Navbar from '../components/Navbar';
+import Home from "../layouts/Home/Home";
+import Footer from "../components/Footer";
+import { createContext } from 'react';
+
+import favicon from '../assets/logo.svg';
 import './App.css';
 
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes, 
+  HashRouter
+} from "react-router-dom";
+
+export const AppContext = createContext({});
+
 function App() {
+  const [theme, setTheme] = useState('dark')
+
+  const onUpdateTheme = () => {
+    const newTheme = theme === 'dark' ? 'light' : 'dark'
+    window.localStorage.setItem('theme', newTheme)
+    setTheme(newTheme)
+  }
+
+  useEffect(() => {
+    const savedTheme = window.localStorage.getItem('theme')
+
+    if (savedTheme) {
+      setTheme(savedTheme)
+    }
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <HelmetProvider>
+    <Router>
+    <HashRouter basename="/">
+      <div className="App">
+      <Helmet>
+        <link rel="shortcut icon" type="image/png" href={favicon} />
+        {theme === 'dark' && (
+          <link rel="stylesheet" type="text/css" href="../static/dark-mode.css" />
+        )}
+      </Helmet>
+        <Navbar onUpdateTheme={onUpdateTheme} theme={theme}/>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+        </Routes>
+        
+        <Footer />
+      </div>
+      </HashRouter>
+    </Router>
+    </HelmetProvider>
   );
 }
 
