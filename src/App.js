7e0c73ad32d4d26e2d9deed092a3b90ed667cec2
Daniel Pérez <dpdaniperezsanz@gmail.com>
2023-05-26 19:29:00 +0100

    fix some issues at deployment

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { Helmet, HelmetProvider } from 'react-helmet-async';
 
-import Navbar from '../components/Navbar';
-import Home from "../layouts/Home/Home";
-import Footer from "../components/Footer";
-import { createContext } from 'react';
+import Navbar from './components/Navbar';
+import Footer from "./components/Footer";
 
-import favicon from '../assets/logo.svg';
+import favicon from './assets/logo.svg';
 import './App.css';
 
 import {
   BrowserRouter as Router,
   Route,
-  Routes, 
-  HashRouter
+  Routes
 } from "react-router-dom";
-
-export const AppContext = createContext({});
 
 function App() {
   const [theme, setTheme] = useState('dark')
@@ -36,26 +30,19 @@ function App() {
   }, [])
 
   return (
-     <HelmetProvider>
-    <Router>
-    <HashRouter basename="/">
-      <div className="App">
-      <Helmet>
-        <link rel="shortcut icon" type="image/png" href={favicon} />
-        {theme === 'dark' && (
-          <link rel="stylesheet" type="text/css" href="../static/dark-mode.css" />
-        )}
-      </Helmet>
-        <Navbar onUpdateTheme={onUpdateTheme} theme={theme}/>
-        <Routes>
-          <Route exact path='/' element={<Home />} />
-        </Routes>
-        
-        <Footer />
-      </div>
-      </HashRouter>
-    </Router>
-    </HelmetProvider>
+      <Router>
+        <div className="App">
+          <link rel="shortcut icon" type="image/png" href={favicon} />
+          {theme === 'dark' && (
+            <link rel="stylesheet" type="text/css" href="../static/dark-mode.css" />
+          )}
+          <Navbar onUpdateTheme={onUpdateTheme} theme={theme}/>
+          <Routes>
+          </Routes>
+          
+          <Footer />
+        </div>
+      </Router>
   );
 }
 
